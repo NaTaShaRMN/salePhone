ca4182990d2fe546f61d180e827fc535a889d53c
thenam153 <noilinhtinh016@gmail.com>
2019-05-18 13:07:02 +0700

    fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,5 +31,15 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         View::share('brand',Brand::all());
         View::share('col',Ex::where('type','1')->take(3)->get());
+        $silde = Ex::where('type','2')->get();
+        if(count($silde)){
+            View::share('silde',$silde[0]);    
+        }else{
+            $silde->image= '';
+            $silde->text = 'Mua hàng của chúng tôi';
+            $silde->link = 'index';
+            View::share('silde',$silde);    
+        }
+        
     }
 }
